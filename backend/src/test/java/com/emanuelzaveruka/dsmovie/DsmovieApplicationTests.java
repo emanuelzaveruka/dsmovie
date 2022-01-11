6a146484274ba67c2b3774a4795f1d239a521bec
Emanuel Zaveruka <emanuelzaveruka@gmail.com>
2022-01-11 16:41:46 -0300

    Project created

---
+++ backend/src/test/java/com/emanuelzaveruka/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emanuelzaveruka.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
